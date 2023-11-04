04ecd244028f5df2721bf52096da658de3ffbafc
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2023-11-04 13:30:01 +0100

    fix

--- handlers/hello.go
+++ handlers/hello.go
@@ -3,10 +3,8 @@ package handlers
 import (
 	"context"
 	"errors"
-	"fmt"
 	protos "github.com/MihajloJankovic/profile-service/protos/main"
 	"github.com/gorilla/mux"
-	"io/ioutil"
 	"log"
 	"mime"
 	"net/http"
@@ -23,41 +21,28 @@ func NewHello(l *log.Logger, cc protos.ProfileClient) *Hello {
 }
 
 func (h *Hello) SetProfile(w http.ResponseWriter, r *http.Request) {
+
 	contentType := r.Header.Get("Content-Type")
 	mediatype, _, err := mime.ParseMediaType(contentType)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
 	if mediatype != "application/json" {
 		err := errors.New("Expect application/json Content-Type")
 		http.Error(w, err.Error(), http.StatusUnsupportedMediaType)
 		return
 	}
-
 	rt, err := DecodeBody(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusAccepted)
 		return
 	}
-	h.l.Println(rt)
-
-	response, err := h.cc.SetProfile(context.Background(), rt)
+	_, err = h.cc.SetProfile(context.Background(), rt)
 	if err != nil {
 		log.Fatalf("RPC failed: %v", err)
 	}
-
-	log.Printf("Response: %v", response)
-
-	//fmt.Fprintf(w, "Hello %s", response.GetFirstname())
-	//fmt.Fprintf(w, "Hello %s", response.String())
-	d, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	fmt.Fprintf(w, "Hello %s", d)
+	w.WriteHeader(http.StatusCreated)
 }
 func (h *Hello) GetProfile(w http.ResponseWriter, r *http.Request) {
 	emaila := mux.Vars(r)["email"]
@@ -66,16 +51,10 @@ func (h *Hello) GetProfile(w http.ResponseWriter, r *http.Request) {
 	response, err := h.cc.GetProfile(context.Background(), ee)
 	if err != nil {
 		log.Fatalf("RPC failed: %v", err)
-	}
-
-	log.Printf("Response: %v", response)
-
-	//fmt.Fprintf(w, "Hello %s", response.GetFirstname())
-	//fmt.Fprintf(w, "Hello %s", response.String())
-	d, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write([]byte("Profile not found"))
 		return
 	}
-	fmt.Fprintf(w, "Hello %s", d)
+	w.WriteHeader(http.StatusOK)
+	RenderJSON(w, response)
 }
--- handlers/helper.go
+++ handlers/helper.go
@@ -6,6 +6,7 @@ import (
 	protos "github.com/MihajloJankovic/profile-service/protos/main"
 	"github.com/google/uuid"
 	"io"
+	"net/http"
 )
 
 func StreamToByte(stream io.Reader) []byte {
@@ -24,28 +25,26 @@ func DecodeBody(r io.Reader) (*protos.ProfileResponse, error) {
 	return &rt, nil
 }
 
+//	func decodeGroupBody(r io.Reader) (*ConfigGroup, error) {
+//		dec := json.NewDecoder(r)
+//		dec.DisallowUnknownFields()
 //
-//func decodeGroupBody(r io.Reader) (*ConfigGroup, error) {
-//	dec := json.NewDecoder(r)
-//	dec.DisallowUnknownFields()
-//
-//	var rt ConfigGroup
-//	if err := json.Unmarshal(StreamToByte(r), &rt); err != nil {
-//		return nil, err
+//		var rt ConfigGroup
+//		if err := json.Unmarshal(StreamToByte(r), &rt); err != nil {
+//			return nil, err
+//		}
+//		return &rt, nil
 //	}
-//	return &rt, nil
-//}
-//
-//func renderJSON(w http.ResponseWriter, v interface{}) {
-//	js, err := json.Marshal(v)
-//	if err != nil {
-//		http.Error(w, err.Error(), http.StatusInternalServerError)
-//		return
-//	}
-//
-//	w.Header().Set("Content-Type", "application/json")
-//	w.Write(js)
-//}
+func RenderJSON(w http.ResponseWriter, v interface{}) {
+	js, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
 
 func createId() string {
 	return uuid.New().String()
