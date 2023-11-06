d6896eca747a0d9742f09fbf2d0d7516a1667f55
Pana77 <101635157+Pana77@users.noreply.github.com>
2023-11-06 21:00:14 +0100

    commit- accommodation service

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/MihajloJankovic/RestService/handlers"
+	protosAcc "github.com/MihajloJankovic/accommodation-service/protos/main"
 	protos "github.com/MihajloJankovic/profile-service/protos/main"
 	//protosAcc "github.com/MihajloJankovic/accommodation-service/protos/main"
 	"github.com/gorilla/mux"
@@ -25,14 +26,24 @@ func main() {
 		panic(err)
 	}
 	defer conn.Close()
+	connAcc, err := grpc.Dial("accommodation-service:9093", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
 	cc := protos.NewProfileClient(conn)
+	acc := protosAcc.NewAccommodationClient(connAcc)
 	hh := handlers.NewPorfilehendler(l, cc)
+	acch := handlers.NewAccommodationHandler(l, acc)
 	router := mux.NewRouter()
 	router.StrictSlash(true)
 
 	router.HandleFunc("/profile/{email}", hh.GetProfile).Methods("GET")
-	router.HandleFunc("/addprofile", hh.SetProfile).Methods("POST")
+	router.HandleFunc("/add-profile", hh.SetProfile).Methods("POST")
 	router.HandleFunc("/update-profile", hh.UpdateProfile).Methods("POST")
+	router.HandleFunc("/accommodation/{email}", acch.GetAccommodation).Methods("GET")
+	router.HandleFunc("/add-accommodation", acch.SetAccommodation).Methods("POST")
+	router.HandleFunc("/update-accommodation", acch.UpdateAccommodation).Methods("POST")
 	// test ee := protos.ProfileRequest{Email: "pera@gmail.com"}
 	// test cc.GetProfile(context.Background(),&ee)
 
