22ed2d0dd1840fb990595aa6f08ee0c0c72d762f
Stefan Milosavljević <bookpurple018@gmail.com>
2023-11-06 19:32:25 +0100

    Dodat auth-service

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"context"
+
+	protosAuth "github.com/MihajloJankovic/Auth-Service/protos/main"
 	"github.com/MihajloJankovic/RestService/handlers"
 	protos "github.com/MihajloJankovic/profile-service/protos/main"
+
 	//protosAcc "github.com/MihajloJankovic/accommodation-service/protos/main"
-	"github.com/gorilla/mux"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/gorilla/mux"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -27,6 +31,13 @@ func main() {
 	defer conn.Close()
 	cc := protos.NewProfileClient(conn)
 	hh := handlers.NewPorfilehendler(l, cc)
+	conn, err := grpc.Dial("auth-service:8082", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	ccAuth := protosAuth.NewProfileClient(con)
+	hhAuth := handlers.NewPorfilehendler(1, ccAuth)
 	router := mux.NewRouter()
 	router.StrictSlash(true)
 
@@ -35,7 +46,8 @@ func main() {
 	router.HandleFunc("/update-profile", hh.UpdateProfile).Methods("POST")
 	// test ee := protos.ProfileRequest{Email: "pera@gmail.com"}
 	// test cc.GetProfile(context.Background(),&ee)
-
+	router.HandleFunc("/register", hhAuth.RegisterUser).Methods("POST")
+	router.HandleFunc("/login", hhAuth.LoginUser).Methods("POST")
 	srv := &http.Server{Addr: ":9090", Handler: router}
 	go func() {
 		log.Println("server starting")
