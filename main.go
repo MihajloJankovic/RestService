c5dbca74fd4b3e5cce3a84cd6c7807b51a53cd07
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2023-11-25 21:50:31 +0100

    Update main.go

--- main.go
+++ main.go
@@ -118,7 +118,7 @@ func main() {
 	router.HandleFunc("/get-all-avaibility", hhava.GetAllbyId).Methods("POST")
 	router.HandleFunc("/check-avaibility", hhava.CheckAvaibility).Methods("POST")
 
-	headersOk := habb.AllowedHeaders([]string{"Content-Type", "Authorization"})
+	headersOk := habb.AllowedHeaders([]string{"Content-Type", "jwt", "Authorization"})
 	originsOk := habb.AllowedOrigins([]string{"http://localhost:4200"}) // Replace with your frontend origin
 	methodsOk := habb.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
