68748dacd603de040a31d410022d3fa942245ebd
Pana77 <101635157+Pana77@users.noreply.github.com>
2023-11-26 12:57:58 +0100

    fix

--- handlers/Reservation.go
+++ handlers/Reservation.go
@@ -52,7 +52,7 @@ func (h *ReservationHandler) SetReservation(w http.ResponseWriter, r *http.Reque
 		http.Error(w, err.Error(), http.StatusForbidden)
 		return
 	}
-	//TODO Add call to avaibility service for check if date is avaiable
+	//TODO Add call to avaibility service for check if date is available
 	_, err = h.acc.SetReservation(context.Background(), rt)
 	if err != nil {
 		log.Printf("RPC failed: %v\n", err)
